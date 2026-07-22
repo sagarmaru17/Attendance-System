28e66b819bfd34ae9a22d2f7b8d77ee9b8ac05a6
Sagar  Maru <sagarmaru.017@gmail.com>
2026-07-22 16:07:00 +0530

    Fix: Use CustomUser model instead of default User in create_superuser command

--- BeQr-Attendance System/core/management/commands/create_superuser.py
+++ BeQr-Attendance System/core/management/commands/create_superuser.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from core.models import CustomUser
 import os
 
 
@@ -12,8 +12,8 @@ class Command(BaseCommand):
         password = os.environ.get('ADMIN_PASSWORD', 'admin123')
 
         # Check if user already exists
-        if User.objects.filter(username=username).exists():
+        if CustomUser.objects.filter(username=username).exists():
             self.stdout.write(self.style.WARNING(f'User {username} already exists'))
         else:
-            User.objects.create_superuser(username=username, email=email, password=password)
+            CustomUser.objects.create_superuser(username=username, email=email, password=password)
             self.stdout.write(self.style.SUCCESS(f'Successfully created superuser {username}'))
